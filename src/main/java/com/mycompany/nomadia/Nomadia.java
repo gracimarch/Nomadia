638a07dc869e3b6a16db11bb2b3f30fd5c564d4b
Graciana <gracianamarch1@gmail.com>
2025-09-26 21:34:57 -0300

    base de datos integrada

--- src/main/java/com/mycompany/nomadia/Nomadia.java
+++ src/main/java/com/mycompany/nomadia/Nomadia.java
@@ -1,5 +1,10 @@
 package com.mycompany.nomadia;
 
+import java.sql.Connection;     /*Representa la conexión a la base de datos*/
+import java.sql.DriverManager;  /*Gestiona los drivers JDBC y abre la conexión*/
+import java.sql.Statement;      /*Permite ejecutar sentencias SQL (CREATE, INSERT, UPDATE, DELETE)*/
+import java.sql.ResultSet;      /*Contiene los resultados de una consulta SELECT*/
+
 public class Nomadia {
 
     public static void main(String[] args) {
