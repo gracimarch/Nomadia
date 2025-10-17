42065db9a3f49a409f3ebf97d4ad9d971b406181
Graciana <gracianamarch1@gmail.com>
2025-10-17 20:32:45 -0300

    clase Administrador eliminada

--- src/main/java/com/mycompany/nomadia/Administrador.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.nomadia;
-
-public class Administrador extends Usuario {
-    
-    public Administrador(String nombre, String email, String telefono) {
-        super(nombre, email, telefono, "Administrador");
-    }
-    
-}
